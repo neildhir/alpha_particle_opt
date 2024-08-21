3fe4432c45558363ad62bcfb43f7211a8ba6484a
mishapadidar <map454@cornell.edu>
2024-08-21 15:36:55 -0400

    set up logCosntrainedExpectedImprovement acqf, and started using a model for the cosntraints

--- src/bo/bo_solver.py
+++ src/bo/bo_solver.py
@@ -5,11 +5,16 @@ import numpy as np
 from typing import Optional, List
 from numpy.typing import ArrayLike
 
+from botorch.models import SingleTaskGP
+from botorch.models.transforms import Normalize, Standardize
 from botorch.acquisition import ExpectedImprovement
-from botorch.acquisition.analytic import LogExpectedImprovement
-from torch import Tensor, vstack, save, from_numpy, rand as torch_rand
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from botorch.acquisition.analytic import LogExpectedImprovement, LogConstrainedExpectedImprovement
+from torch import Tensor, vstack, save, from_numpy, rand as torch_rand, topk, float64
 from botorch.fit import fit_gpytorch_mll
 from torch.autograd import grad as torch_grad
+from botorch.optim import optimize_acqf
+
 from scipy.optimize import OptimizeResult, minimize as scipy_minimize
 
 from src.bo.initialisers import gen_batch_initial_conditions_nonlinear
@@ -39,9 +44,12 @@ class BoSolver:
         ----------
         train_X : Array
             Input features (training data, Fourier coefficients)
-        train_Y : Array
-            Target (expected energy loss for this shape of the plasma boundary represented by the Fourier coefficients)
-             shape (training data, 1)
+        train_Y : 2d, Array
+            First column is the objective evaluations: (expected energy loss for this shape of the plasma boundary represented by the Fourier coefficients)
+            Remaining columns are constraint evaluations: (field strength evaluation). There can be any number
+            of constraints, however, we will be building GP models of each one. So, it is best for computational complexity 
+            to have less constraints. The constraints should be passed in the same order to the solve() function.
+            shape (training data, n_constraints)
         max_iter : int, optional
             Number of operations, by default 5 -- will be replaced with a stopping criterion
         verbose : bool, optional
@@ -60,121 +68,154 @@ class BoSolver:
         self.SMOKE_TEST = SMOKE_TEST
         self.num_restarts = num_restarts
         self.raw_samples = raw_samples
+        self.q = 1 # q=1 for EI acqf
 
-    def sample_initial_points(self, acq_func: callable, bounds: Tensor,
-                                  nonlinear_constraint: Optional[callable] = None):
+        self.n_obj_con = np.shape(train_Y)[1] 
+        self.n_constraints = self.n_obj_con - 1
+        # index of objective/constraints in train_Y
+        self.objective_index = 0
+        self.constraint_indexes = range(1, self.n_obj_con)
+
+
+    def build_singleTaskGP(self, train_X, train_Y, bounds):
         """
-        Sample warm starts for optimizer.
+        Build a GP model for the objectives and constraints. 
+        It is assumed that the objective and constraints are evaluted
+        at the same set of points.
 
-        acq_func: callable,
-            handle to the acquisition function.
-        bounds: Tensor,
-            2 x dim_x tensor of lower and then upper bounds.
-        nonlinear_constraints: callable or None,
-            A constraint function for constraints of the form c(x) >= 0. The function 
-            has an argument X that takes in a 2d Tensor of points (n_points, dim_x)
-            and returns an (n_points, 1) tensor of constraint evalautions. The constraint 
-            must be single-valued -- if you have a vector valued constraint take the min.
-
+        inputs
+        ------
+        train_X: torch tensor, shape (n, dim_x)
+        train_Y: torch tensor, shape (n, n_constraints)
+        bounds: torch tensor, shape (2, dim_x)
+            bound constraints on x.
+        
         return
-        --------
-        X_cand: array, shape (num_restarts, dim_x)
-            initial points for optimizer.
+        ------
+        mll: marginal log likelihood.
+        model: GP model.
         """
-        ## TODO: expose q as a parameter that we force to be 1, so that shaping everywhere is consistent
-        #X_cand = gen_batch_initial_conditions_nonlinear(acq_func, q=1, num_restarts=self.num_restarts,            
-        #                                        raw_samples=self.raw_samples,
-        #                                        nonlinear_constraint=nonlinear_constraint,
-        #                                        bounds=bounds
-        #                                        ) # (num_restarts, q, dim_x)
-  
-        # TODO: use rejection sampling here
-        # uniformly sample the region
-        dim_x = bounds.shape[1]
-        unif = torch_rand((self.num_restarts, 1, dim_x)) # (num_restarts, q, dim_x)
-        X_cand =  (bounds[1] - bounds[0])*unif + bounds[0] # (num_restarts, q, dim_x)
 
-        X_cand = X_cand.detach().numpy()
-        X_cand = np.squeeze(X_cand, axis=1) # (num_restarts, dim_x)
-        return X_cand
+        # TODO: use a noise model for the modB constraints
+        # Build surrogate model, kernel uses ARD by default
+        model = SingleTaskGP(
+            train_X=train_X,
+            train_Y=train_Y,
+            outcome_transform=Standardize(m=self.n_obj_con),
+            input_transform=Normalize(d=train_X.size(1), bounds=bounds),
+        )
 
-    def optimize_acquisition(self, objective: callable, 
-                               X: Tensor, acq_func, bounds: List[tuple], constraints=[],
-                                method="SLSQP", options: dict={},
-                                ):
+        mll = ExactMarginalLogLikelihood(model.likelihood, model)
+        return mll, model
+
+    # def sample_initial_points(self, acq_func: callable, bounds: Tensor):
+    #     """
+    #     Sample warm starts for optimizer.
+
+    #     acq_func: callable,
+    #         handle to the acquisition function.
+    #     bounds: Tensor,
+    #         2 x dim_x tensor of lower and then upper bounds.
+
+    #     return
+    #     --------
+    #     X_cand: array, shape (num_restarts, dim_x)
+    #         initial points for optimizer.
+    #     """
+    #     # TODO: use rejection sampling here or the precomputed samples
+
+    #     # uniformly sample the region
+    #     dim_x = bounds.shape[1]
+    #     unif = torch_rand((self.raw_samples, 1, dim_x)) # (raw_samples, q, dim_x)
+    #     X_cand =  (bounds[1] - bounds[0])*unif + bounds[0] # (raw_samples, q, dim_x)
+
+    #     # TODO: test this
+    #     # evaluate the acq_func
+    #     qx = acq_func(X_cand) # (raw_samples, 1)
+    #     _, top_idx = topk(-qx, self.num_restarts)  
+    #     X_cand = X_cand[top_idx] # (num_restarts, q, dim_x)
+
+    #     X_cand = X_cand.detach().numpy()
+    #     X_cand = np.squeeze(X_cand, axis=1) # (num_restarts, dim_x)
+    #     return X_cand
+
+    # def optimize_acquisition(self, acqf, bounds):
+    #     """
+    #     Generate samples and optimize the acquisition function
+
+    #     Parameters
+    #     ----------
+    #     acqf: acquisition function
+    #     bounds: bound constraints for torch.
+
+    #     Return
+    #     ----------
+    #     new_x: Tensor,
+    #         2d tensor of shape (num_restarts, dim_x) of new iterates.
+    #     new_f: Tensor,
+    #         2d tensor of shape (num_restarts, 1) of evaluations of new_x.
+    #     """
+    #     # (q, dim_x)
+    #     candidates, _ = optimize_acqf(acqf, bounds, q=1,
+    #                                     num_restarts=self.num_restarts,
+    #                                     raw_samples=self.raw_samples) 
+
+    #     return candidates 
+
+    def evaluate_candidates(self, X, objective, constraints):
         """
-        Generate samples and optimize the acquisition function
+        Evaluate the objective and constraint values of the candidates, X. 
 
-        Parameters
-        ----------
-        objective: callable, 
-            function handle to the objective
-        acq_func: 
-            A torch acquisition function
-        bounds: list 
-            list of tuples of lower and upper bounds, i.e. [(0.0, 1.0), ..., (-1.0, 4.0)]
-        constraints: list 
-            containing any scipy.NonlinearConstraint and scipy.LinearConstraint instances.
-        method: str,
-            optimization method for the scipy minimize solver
-        options: dict,
-            dictionary of options to pass to the scipy minimize solver
-
-        Return
-        ----------
-        new_x: Tensor,
-            2d tensor of shape (num_restarts, dim_x) of new iterates.
-        new_f: Tensor,
-            2d tensor of shape (num_restarts, 1) of evaluations of new_x.
+        inputs
+        ------
+        X: torch tensor, shape (n_points, dim_x)
+        
+        return
+        ------
+        Y: torch tensor, shape (n_points, n_obj_con)
         """
-        def acquisition_wrapper(x):
-            # TODO: set up gradient with autodiff
-            y = from_numpy(x).reshape((1,1,-1))
-            return acq_func.forward(y).detach().numpy().item()
+        n_x = X.shape[0]
+        Y = np.zeros((n_x, self.n_obj_con))
 
-        # multi-start optimize the acquisition
-        candidates = np.zeros(np.shape(X))
-        for ii, x0 in enumerate(X):
-            # TODO: set up joint optimization of all restarts
-            # TODO: set up constraint gradients
-            res = scipy_minimize(acquisition_wrapper, x0=x0, bounds=bounds,
-                                 constraints=constraints,
-                                 method=method, options=options)
-            candidates[ii] = x0
+        for ii, x in enumerate(X):
+            x = x.numpy().flatten()
 
-        # Observe new values
-        obj_candidates = np.array([objective(x) for x in candidates])
+            Y[ii, self.objective_index] = objective(x)
 
-        return from_numpy(candidates), from_numpy(obj_candidates.reshape((-1,1)))
+            if self.n_constraints > 0:
+                Y[ii, self.constraint_indexes] = [cc.fun(x) for cc in constraints]
+                
+        Y = from_numpy(Y.reshape((-1, self.n_obj_con)))
 
+        return Y
 
-    def compound_nonlinear_constraints(self, X: Tensor, constraints=[]) -> Tensor:
-        """
-        Function to compute the compound nonlinear constraint for the acquisition function
-        optimization.
+    # def compound_nonlinear_constraints(self, X: Tensor, constraints=[]) -> Tensor:
+    #     """
+    #     Function to compute the compound nonlinear constraint for the acquisition function
+    #     optimization.
 
-        Parameters
-        ----------
-        X : Tensor
-            2D array of candidate Fourier coefficients (candidates x # Fourier coefficients)
+    #     Parameters
+    #     ----------
+    #     X : Tensor
+    #         2D array of candidate Fourier coefficients (candidates x # Fourier coefficients)
 
-        Returns
-        -------
-        Tensor
-            (candidates x 1) tensor of constraint values. One value per point that is 
-            positive if the point is feasible, and negative otherwise.  
-        """
-        X = X.numpy()
-        # reformat the nlc into c(x) >= 0
-        acqf_nonlinear_inequality_constraints = [(lambda x: cc.fun(x) - cc.lb) for cc in constraints]
-        acqf_nonlinear_inequality_constraints += [(lambda x: cc.ub - cc.fun(x)) for cc in constraints]
+    #     Returns
+    #     -------
+    #     Tensor
+    #         (candidates x 1) tensor of constraint values. One value per point that is 
+    #         positive if the point is feasible, and negative otherwise.  
+    #     """
+    #     X = X.numpy()
+    #     # reformat the nlc into c(x) >= 0
+    #     acqf_nonlinear_inequality_constraints = [(lambda x: cc.fun(x) - cc.lb) for cc in constraints]
+    #     acqf_nonlinear_inequality_constraints += [(lambda x: cc.ub - cc.fun(x)) for cc in constraints]
 
-        nlc = np.zeros(len(X))
-        for ii, xx in enumerate(X):
-            # constraints are assumed to be of type c(x) >= 0
-            cons = np.array([cc(xx) for cc in acqf_nonlinear_inequality_constraints]).flatten()
-            nlc[ii] = np.min(cons)
-        return from_numpy(nlc.reshape((-1, 1)))
+    #     nlc = np.zeros(len(X))
+    #     for ii, xx in enumerate(X):
+    #         # constraints are assumed to be of type c(x) >= 0
+    #         cons = np.array([cc(xx) for cc in acqf_nonlinear_inequality_constraints]).flatten()
+    #         nlc[ii] = np.min(cons)
+    #     return from_numpy(nlc.reshape((-1, 1)))
 
 
     def solve(self, objective, x0, bounds, constraints, method="SLSQP", options={}):
@@ -205,70 +246,85 @@ class BoSolver:
         # TODO: we arent doing anything with x0 yet (include in evals?)
         
         train_X, train_Y = self.train_X, self.train_Y
-        verbose = self.verbose
 
         # reformat the bounds for BO
         torch_bounds = from_numpy(np.array(bounds).T) # 2 x d
 
-        # Build initial candidates generator for the acquisition function
-        ic_nonlinear_inequality_constraints = partial(self.compound_nonlinear_constraints,
-                                                      constraints=constraints)
+        # constraints for optimizing the acquisition function
+        acqf_constraints = {}
+        for ii, cc in enumerate(constraints):
+            idx = self.constraint_indexes[ii]
+            lb = cc.lb if np.isfinite(cc.lb) else None
+            ub = cc.ub if np.isfinite(cc.ub) else None
+            acqf_constraints[idx] = [lb, ub]
 
-        mll, model = build_surrogate_model(train_X, train_Y, torch_bounds)
+        mll, model = self.build_singleTaskGP(train_X, train_Y, torch_bounds)
+
+        # TODO: remove
+        # model = SingleTaskGP(
+        #     train_X=train_X,
+        #     train_Y=train_Y[:,:1],
+        #     outcome_transform=Standardize(m=1),
+        #     input_transform=Normalize(d=train_X.size(1), bounds=torch_bounds),
+        # )
+        # mll = ExactMarginalLogLikelihood(model.likelihood, model)
 
         for i in range(self.max_iter):
+            t0_loop = time.monotonic()
 
+            if self.verbose:
+                print('\nFitting model:')
             t0 = time.monotonic()
 
-            if self.verbose:
-                print('Fitting model:')
-            tt0 = time.monotonic()
-            # Fit model with new data: D =  D_old \cup D_new
+            # Fit model with new data
             fit_gpytorch_mll(mll)
-            tt1 = time.monotonic()
+
+            t1 = time.monotonic()
             if self.verbose:
-                print('--> time', tt1 - tt0)
+                print('--> time', t1 - t0)
 
             # Use best_f (expected energy loss) observed so far
-            ei = LogExpectedImprovement(model, best_f=train_Y.min(), maximize=False)
-
-            if self.verbose:
-                print('Generating candidates:')
-            tt0 = time.monotonic()
-            X_cand = self.sample_initial_points(ei, bounds=torch_bounds,
-                                                    nonlinear_constraint=ic_nonlinear_inequality_constraints)
-            tt1 = time.monotonic()
-            if self.verbose:
-                print('--> time', tt1 - tt0)
+            acqf = LogConstrainedExpectedImprovement(model, best_f=train_Y[:,self.objective_index].min(),
+                                                     objective_index = self.objective_index,
+                                                     constraints = acqf_constraints,
+                                                     maximize=False)
+            # acqf = ExpectedImprovement(model, best_f=train_Y[:,self.objective_index].min(),
+            #                                          maximize=False)
 
             if self.verbose:
                 print('Optimizing acq_func:')
-            tt0 = time.monotonic()
-            new_x, new_f = self.optimize_acquisition(objective, 
-                                                       X=X_cand, 
-                                                       acq_func=ei, 
-                                                       bounds=bounds, 
-                                                       constraints=constraints,
-                                                       method=method, 
-                                                       options=options,
-                                                       )
-            tt1 = time.monotonic()
-            if self.verbose:
-                print('--> time', tt1 - tt0)
+            t0 = time.monotonic()
 
+            new_x, _ = optimize_acqf(acqf, torch_bounds.float(), q=self.q,
+                                            num_restarts=self.num_restarts,
+                                            raw_samples=self.raw_samples) # (q, dim_x)
+            
+            t1 = time.monotonic()
+            if self.verbose:
+                print('--> time', t1 - t0)
+
+            if self.verbose:
+                print('Evaluating candidate points:')
+            t0 = time.monotonic()
+
+            new_f = self.evaluate_candidates(new_x, objective, constraints)
+
+            t1 = time.monotonic()
+            if self.verbose:
+                print('--> time', t1 - t0)
+
+            # update the dataset
             train_X = vstack([train_X, new_x])
             train_Y = vstack([train_Y, new_f])
 
             # Re-build model with new data, ready for fitting on next iteration
-            mll, model = build_surrogate_model(
-                train_X, train_Y, torch_bounds
-            )  # TODO: we can prime the model with the state_dict insted of re-building it each time, faster.
+            mll, model = self.build_singleTaskGP(train_X, train_Y, torch_bounds) # TODO: we can prime the model with the state_dict insted of re-building it each time, faster.
 
-            t1 = time.monotonic()
-            best_f = train_Y.min().item()
+            t1_loop = time.monotonic()
+            best_f = train_Y[:,self.objective_index].min().item()
             if self.verbose:
                 print(
-                    f"\nIteration {i}: best objective value = " f"{best_f:>4.2f}, " f"time = {t1-t0:>4.2f}.",
+                    f"\nIteration {i}: best objective value = " f"{best_f:>4.4f}, " f"time = {t1_loop-t0_loop:>4.2f}.",
                 )
             else:
                 print(".", end="")
@@ -283,7 +339,7 @@ class BoSolver:
         timestamp = time.strftime("%Y%m%d%H%M%S")  # e.g. 20210909123456
         save(save_results, f"optimization_results_{timestamp}.pth")
 
-        idx_opt = train_Y.argmin()
+        idx_opt = train_Y[:,self.objective_index].argmin()
         result = OptimizeResult()
         result.x = train_X[idx_opt]
         result.fun = train_Y[idx_opt]
@@ -293,23 +349,30 @@ class BoSolver:
 
 
 if __name__ == "__main__":
+
     from scipy.optimize import NonlinearConstraint
     
     dim = 2
     x0 = np.random.randn(dim)
-    objective = lambda x: x @ x
+    objective = lambda x: (x - 1.2) @ (x-1.2) #+ 0.3*np.sin(x @ x)
+    constraint = lambda x: x.sum()
     bounds = [(0, 10), (0,10)]
-    constraints = [NonlinearConstraint(lambda x: x.sum(), 0.0, np.inf)]
+
+    constraints = [NonlinearConstraint(constraint, -np.inf, 1.0)]
 
     train_X = 10*np.random.uniform(size=(10,dim))
-    train_Y = np.array([objective(x) for x in train_X]).reshape((-1,1))
+    train_Y = np.array([[objective(x), constraint(x)] for x in train_X]).reshape((-1,2))
+
+    print(train_X)
+    print(train_Y)
 
     solver = BoSolver(train_X,
                  train_Y,
-                 max_iter=10,
+                 max_iter=20,
                  verbose=True
-                 )   
+                 )
 
     res = solver.solve(objective, x0, bounds, constraints, 
                        method='SLSQP', options={'maxiter':20, 'ftol':1e0})
+    print("")
     print(res)
